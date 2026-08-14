2e68de2a3a83af22892aff21e37c5d60d8b2e65a
Darren <darren.lim.off@gmail.com>
2026-08-14 15:21:07 +0800

    Level-1 customise toot to repeat what I say

--- src/main/java/Toot.java
+++ src/main/java/Toot.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Runs the Toot chatbot application.
  */
@@ -8,14 +10,27 @@ public class Toot {
                 + "  | |/ _ \\ / _ \\| __|\n"
                 + "  | | (_) | (_) | |_\n"
                 + "  |_|\\___/ \\___/ \\__|\n";
-        String horizontalLine = "____________________________________________________________";
+        String horizontalLine = "⋆｡°✩ ──────────────────────────────────────────────── ✩°｡⋆";
 
         System.out.println(horizontalLine);
         System.out.print(banner + "\n");
-        System.out.println("Hewwo! I'm Toot! :3");
-        System.out.println("Whatchu need? I twy my bestest! (•̀ᴗ•́)و");
+        System.out.println("Hewwo!! I'm Toot, ur teeny-tiny computey baby! ૮₍ ˶•⤙•˶ ₎ა");
+        System.out.println("Gib me a command... Toot do a BIG twy!! (•̀ᴗ•́)و");
         System.out.println(horizontalLine + "\n");
-        System.out.println("Otay bye-bye! Come back soon! (｡•́‿•̀｡)ﾉ");
-        System.out.println(horizontalLine);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            System.out.println(horizontalLine);
+
+            if (command.equals("bye")) {
+                System.out.println("Otay bye-bye! Toot go eepy now... zZz (｡-ω-)ﾉ");
+                System.out.println(horizontalLine);
+                break;
+            }
+
+            System.out.println("Toot hearded: \"" + command + "\"  (｡•̀ᴗ-)✧");
+            System.out.println(horizontalLine + "\n");
+        }
     }
 }
