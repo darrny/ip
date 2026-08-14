c899b8b062d5ca54894fd03350d367f26d199610
Darren <darren.lim.off@gmail.com>
2026-08-14 15:49:38 +0800

    Introduce Task domain model
    Move task descriptions, completion state, status formatting, and state transitions into a dedicated Task class. Refactor the command loop to use Task objects so related data cannot become misaligned and future task behavior has a clear home.

--- src/main/java/Toot.java
+++ src/main/java/Toot.java
@@ -19,8 +19,7 @@ public class Toot {
         System.out.println(horizontalLine + "\n");
 
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];
-        boolean[] isDone = new boolean[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         while (scanner.hasNextLine()) {
@@ -36,8 +35,7 @@ public class Toot {
             if (command.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    String status = isDone[i] ? "X" : " ";
-                    System.out.println((i + 1) + ".[" + status + "] " + tasks[i]);
+                    System.out.println((i + 1) + "." + tasks[i]);
                 }
             } else if (command.equals("mark") || command.startsWith("mark ")) {
                 String taskNumberText = command.substring("mark".length()).trim();
@@ -47,9 +45,9 @@ public class Toot {
                     if (taskIndex < 0 || taskIndex >= taskCount) {
                         System.out.println("Toot can't find that task! Try a number from the list. (｡•́︿•̀｡)");
                     } else {
-                        isDone[taskIndex] = true;
+                        tasks[taskIndex].markAsDone();
                         System.out.println("Nice! I've marked this task as done:");
-                        System.out.println("  [X] " + tasks[taskIndex]);
+                        System.out.println("  " + tasks[taskIndex]);
                     }
                 } catch (NumberFormatException exception) {
                     System.out.println("Toot needs a task number after 'mark'. (｡•́︿•̀｡)");
@@ -62,15 +60,15 @@ public class Toot {
                     if (taskIndex < 0 || taskIndex >= taskCount) {
                         System.out.println("Toot can't find that task! Try a number from the list. (｡•́︿•̀｡)");
                     } else {
-                        isDone[taskIndex] = false;
+                        tasks[taskIndex].markAsNotDone();
                         System.out.println("OK, I've marked this task as not done yet:");
-                        System.out.println("  [ ] " + tasks[taskIndex]);
+                        System.out.println("  " + tasks[taskIndex]);
                     }
                 } catch (NumberFormatException exception) {
                     System.out.println("Toot needs a task number after 'unmark'. (｡•́︿•̀｡)");
                 }
             } else {
-                tasks[taskCount] = command;
+                tasks[taskCount] = new Task(command);
                 taskCount++;
                 System.out.println("Toot addeded: \"" + command + "\"  (｡•̀ᴗ-)✧");
             }
