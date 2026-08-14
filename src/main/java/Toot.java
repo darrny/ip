1b67d4a16592c0ff7b26fcb5a03cf4563a67261b
Darren <darren.lim.off@gmail.com>
2026-08-14 14:47:51 +0800

    Level-0: rename from Duke to Toot

---
+++ src/main/java/Toot.java
@@ -0,0 +1,13 @@
+/**
+ * Runs the Toot chatbot application.
+ */
+public class Toot {
+    public static void main(String[] args) {
+        String banner = " _____           _\n"
+                + "|_   _|__   ___ | |_\n"
+                + "  | |/ _ \\ / _ \\| __|\n"
+                + "  | | (_) | (_) | |_\n"
+                + "  |_|\\___/ \\___/ \\__|\n";
+        System.out.println(banner);
+    }
+}
